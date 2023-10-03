5499108621e0fffe29a8d7aba4ca208f3eb990a8
Valentina De Mite <valesakura97@gmail.com>
2023-10-03 17:11:19 +0200

    added header

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,56 @@
-<script></script>
+<script>
+import { state } from "./state.js";
 
-<template></template>
+export default {
+  name: "App",
+  data() {
+    return {
+      state,
+    };
+  },
+};
+</script>
 
-<style></style>
+<template>
+  <body>
+    <img class="logo" src="./assets/img/Logo.jpg" alt="" />
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <div class="container d-flex justify-content-end">
+        <div class="nav navbar-nav">
+          <a class="nav-item nav-link active" href="#" aria-current="page"
+            >Home</a
+          >
+          <a class="nav-item nav-link" href="#">About</a>
+          <a class="nav-item nav-link" href="#">Academics</a>
+          <a class="nav-item nav-link" href="#">Courses</a>
+          <a class="nav-item nav-link" href="#">News</a>
+        </div>
+      </div>
+    </nav>
+
+    <!-- /header -->
+
+    <main>
+      <div class="jumbotron"></div>
+    </main>
+
+    <!-- /main -->
+
+    <!-- /footer -->
+  </body>
+</template>
+
+<style scoped>
+.logo {
+  width: 8%;
+  position: absolute;
+  top: 0;
+  left: 20%;
+  z-index: 99;
+}
+
+body {
+  position: relative;
+  font-family: "Gabarito", cursive;
+}
+</style>
